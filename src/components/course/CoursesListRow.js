67241e8d9972753b1bb4f84dc4c3abf18e4f2d2b
Jose Manuel Heredia Hidalgo <jmanuelh@gmail.com>
2017-07-28 00:04:09 -0700

    Course Manager Page

--- src/components/course/CoursesListRow.js
+++ src/components/course/CoursesListRow.js
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react';
+import {Link} from 'react-router';
 
 const CourseListRow = ({course}) => {
     return(
         <tr>
-            <td> </td>
-            <td> {course.title} </td>
+            <td> <a href={course.watchHref} target="_blank"> Watch </a> </td>
+            <td> <Link to={'course/' + course.id}> {course.title} </Link> </td>
             <td> {course.authorId} </td>
             <td> {course.category} </td>
             <td> {course.length} </td>
